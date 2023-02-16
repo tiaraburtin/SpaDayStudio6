5d632f12847fbdbba17929ca4c746d7e2702f39d
Courtney Frey <courtneyb.frey@gmail.com>
2023-02-16 17:04:17 -0600

    starter code added

---
+++ SpaDay6/Controllers/SpaController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SpaDay6.Controllers
+{
+    public class SpaController : Controller
+    {
+        public bool CheckSkinType(string skinType, string facialType)
+        {
+
+            if (facialType != "Microdermabrasion")
+            {
+                if (skinType == "oily" && facialType != "Rejuvenating")
+                {
+                    return false;
+                }
+                else if (skinType == "combination" && (facialType != "Rejuvenating" || facialType != "Enzyme Peel"))
+                {
+                    return false;
+                }
+                else if (skinType == "dry" && facialType != "Hydrofacial")
+                {
+                    return false;
+                }
+            }
+
+            return true;
+
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/spa")]
+        public IActionResult Menu(string name, string skintype, string manipedi)
+        {
+            List<string> facials = new List<string>()
+            {
+                "Microdermabrasion", "Hydrofacial", "Rejuvenating", "Enzyme Peel"
+            };
+
+            List<string> appropriateFacials = new List<string>();
+            for (int i = 0; i < facials.Count; i++)
+            {
+                if (CheckSkinType(skintype, facials[i]))
+                {
+                    appropriateFacials.Add(facials[i]);
+                }
+            }
+            return View();
+        }
+    }
+}
+
---
+++ SpaDay6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SpaDay6.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
