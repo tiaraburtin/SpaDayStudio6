84da5c88f832e0cf5634362cc09887d9b6aca5c4
Tiara <tburtin1019@gmail.com>
2023-03-30 14:12:16 -0500

    did it

---
+++ SpaDay6/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaDay6.Models;
+
+namespace SpaDay6.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("user/add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("user")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {  
+            if (newUser.Password == verify)
+            {
+                ViewBag.User = newUser.Username;
+                return View("Index");
+            }
+            else
+            {
+                return Redirect("user/add");
+            }
+        }
+    }
+}
---
+++ SpaDay6/Models/User.cs
@@ -0,0 +1,21 @@
+﻿namespace SpaDay6.Models
+{
+    public class User
+    {
+ 
+
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string VerifyPassword { get; set; }
+
+        public User(string username, string email, string password, string verifyPassword)
+        {
+            Username = username;
+            Email = email;
+            Password = password;
+            VerifyPassword = verifyPassword;
+        }
+
+    }
+}
